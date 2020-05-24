92b669b9eef9be5a4a08a6404407f588761f316a
wiliancalora <wiliancalora@evoluaeducacao.com.br>
2020-05-24 14:14:03 -0300

    Testes de agendamento

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,4 +1,5 @@
 import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmensDTO';
@@ -9,8 +10,8 @@ class AppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(
-      appointments => appointments.date === date,
+    const findAppointment = this.appointments.find(appointments =>
+      isEqual(appointments.date, date),
     );
 
     return findAppointment;
