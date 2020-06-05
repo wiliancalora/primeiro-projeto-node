1898589771c99622b8c3f78e15ead40abb7089bf
wiliancalora <wiliancalora@evoluaeducacao.com.br>
2020-06-05 05:49:02 -0300

    Rota e controller de perfil

---
+++ src/modules/users/services/ShowProfileService.spec.ts
@@ -0,0 +1,36 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import ShowProfileService from './ShowProfileService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let showProfile: ShowProfileService;
+
+describe('UpdateProfile', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    showProfile = new ShowProfileService(fakeUsersRepository);
+  });
+
+  it('should be able show the profile', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'jhondoe@example.com',
+      password: '123456',
+    });
+
+    const profile = await showProfile.execute({
+      user_id: user.id,
+    });
+
+    expect(profile.name).toBe('John Doe');
+    expect(profile.email).toBe('jhondoe@example.com');
+  });
+
+  it('should be able show the profile', async () => {
+    expect(
+      showProfile.execute({
+        user_id: 'non-existing-user-id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -34,6 +34,16 @@ describe('UpdateProfile', () => {
     expect(updateUser.email).toBe('jhontre@example.com');
   });
 
+  it('should be able show the profile', async () => {
+    expect(
+      updateProfile.execute({
+        user_id: 'non-existing-user-id',
+        name: 'Test',
+        email: 'teste@example.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
   it('should not be able to change to another user email', async () => {
     await fakeUsersRepository.create({
       name: 'John Doe',
